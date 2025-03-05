c0d57515d8e4c91ab6c25bfdfc84523af0836257
Nihaa05 <148804343+Nihaa05@users.noreply.github.com>
2025-03-04 19:23:37 -0600

    Add files via upload

---
+++ 1431_KidsWcandies.py
@@ -0,0 +1,5 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandies = max(candies)
+
+        return[(curNum + extraCandies >= maxCandies) for  curNum in candies]
